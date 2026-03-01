a4cd9b6037adff9ed8b3e0ce5a09f94f11b1f518
Sergio <TU_CORREO>
2026-03-01 03:23:28 -0600

    Replace bcrypt with bcryptjs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
